ec9c6ca7e47fff021ffd68ca92fdb7489739e48c
MatiasGottler <131204273+MatiasGottler@users.noreply.github.com>
2025-02-01 11:28:15 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,343 @@
+const translations = {
+    es: {
+        title: "TrendMakers",
+        subtitle: "Your digital Partner",
+        navInicio: "Inicio",
+        navAcerca: "¿Por qué elegirnos?",
+        navServicios: "Nuestros Servicios",
+        navContacto: "Agenda una Llamada",
+        navEquipo: "Nuestro Equipo",
+        introTitle: "Bienvenidos a TrendMakers",
+        introText: "Ofrecemos soluciones de marketing digital personalizadas para tu negocio.",
+        aboutTitle: "Acerca de Nosotros",
+        aboutText: "Tomamos tu proyecto como si fuera nuestro.<br><br>Para eso empezamos por aprender y escuchar. Entendemos tu problemática y te presentamos recursos y acciones para llegar a donde quieres.<br><br>Somos ágiles y nos enfocamos en los resultados. Buscamos cumplir objetivos e ir por nuevos desafíos.<br><br>Lo hacemos combinando marketing digital, diseño, programación, IA y consultoría, de principio a fin. Desde que nos contactas hasta que funciona.",
+        servicesTitle: "Nuestros Servicios",
+        servicesText: "Ofrecemos una amplia gama de servicios de marketing digital...",
+        teamTitle: "Nuestro equipo está conformado por:",
+        teamDetails: `
+            <div class="team-card">
+                <h3>Consultores en Marketing</h3>
+                <p>Brindan asesoramiento estratégico para hacer crecer tu negocio.</p>
+            </div>
+            <div class="team-card">
+                <h3>Diseñadores UX/UI, Gráficos y Multimedia</h3>
+                <p>Encargados de crear experiencias digitales innovadoras y amigables.</p>
+            </div>
+            <div class="team-card">
+                <h3>Analistas de Datos</h3>
+                <p>Expertos en interpretar métricas para impulsar la toma de decisiones informadas.</p>
+            </div>
+            <div class="team-card">
+                <h3>Especialistas en Campañas Digitales</h3>
+                <p>Optimizan campañas publicitarias para obtener los mejores resultados.</p>
+            </div>
+            <div class="team-card">
+                <h3>Desarrolladores Web</h3>
+                <p>Capaces de crear sitios y plataformas digitales de alto rendimiento.</p>
+            </div>
+            <div class="team-card">
+                <h3>Especialistas en SEO</h3>
+                <p>Mejoran el posicionamiento orgánico y aumentan la visibilidad online de tu negocio.</p>
+            </div>
+            <div class="team-card">
+                <h3>Especialistas en Business Intelligence</h3>
+                <p>Ayudan a transformar datos en información valiosa y accionable.</p>
+            </div>
+            <div class="team-card">
+                <h3>Community Managers</h3>
+                <p>Expertos en la gestión de redes para aumentar la presencia y el compromiso de tu marca.</p>
+            </div>
+        `,
+        contactTitle: "Agenda una Llamada 1 a 1",
+        scheduleCall: "Agenda una Llamada",
+        problemaButton: "Contanos tu Proyecto",
+        popupTitle: "Contanos tu Proyecto",
+        labelEmail: "Correo Electrónico:",
+        labelProblema: "Proyecto:",
+        submitButton: "Enviar",
+        footerText: "&copy; 2023 TrendMakers. Todos los derechos reservados.",
+        aboutUsText: "Somos una agencia de marketing a nivel global que se dedica a brindar servicios de excelencia a clientes de todas partes del mundo. Con un equipo de expertos en diversas áreas del marketing digital, nos especializamos en crear estrategias personalizadas que se adaptan a las necesidades específicas de cada negocio. Nuestra misión es impulsar el crecimiento y éxito de nuestros clientes a través de soluciones innovadoras y efectivas. Desde la gestión de redes sociales hasta el desarrollo de campañas publicitarias integrales, estamos comprometidos con la excelencia y la satisfacción del cliente en cada proyecto que emprendemos.",
+        serviceDetails: `
+            <div class="service-card">
+                <img src="logo1.png" alt="Marketing & Tecnología">
+                <h3>Marketing & Tecnología</h3>
+                <p>Usamos plataformas tecnológicas y desarrollos a medida para optimizar los procesos de Marketing y Ventas, implementando automatización en los ciclos comerciales.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Leads">
+                <h3>Leads</h3>
+                <p>Generación de Prospectos/Leads para plataformas SaaS y empresas de servicios. Implementación e Integración con CRM.</p>
+            </div>
+            <div class="service-card">
+                <img src="IA.jpg" alt="CHATGPT - IA">
+                <h3>CHATGPT - IA</h3>
+                <p>Potenciamos tu empresa capacitando en ChatGPT y otras herramientas de Inteligencia Artificial en el día a día a todos los sectores y empleados.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Diseño/Comunicación">
+                <h3>Diseño/Comunicación</h3>
+                <p>Diseño y maquetado de Webs, Landing Pages, WordPress y Newsletters. Diseño de piezas gráficas para redes sociales, publicidad, eventos. Community Management.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="E-Commerce">
+                <h3>E-Commerce</h3>
+                <p>Operamos nuestros propios e-commerce. Sabemos cómo hacerlo. Te acompañamos desde la idea hasta la puesta en marcha en todas las etapas del proyecto.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Consultoría">
+                <h3>Consultoría</h3>
+                <p>Brindamos consultoría en Marketing & Tecnología (infraestructura o software) para tus proyectos.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Datos y Analítica">
+                <h3>Datos y Analítica</h3>
+                <p>Convertimos datos en información valiosa y accionable. Utilizamos herramientas avanzadas para interpretar métricas y generar insights que impulsen decisiones estratégicas.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Estudios de Mercado">
+                <h3>Estudios de Mercado</h3>
+                <p>Realizamos estudios de mercado exhaustivos para entender tendencias y necesidades de tu público objetivo. Proveemos análisis detallados para identificar oportunidades y desarrollar estrategias efectivas.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Desarrollo WEB">
+                <h3>Desarrollo WEB</h3>
+                <p>Desarrollamos sitios y plataformas digitales de alto rendimiento, adaptados a tus necesidades. Desde el diseño inicial hasta la implementación final, garantizamos una experiencia de usuario excepcional y un rendimiento óptimo.</p>
+            </div>
+        `,
+        contactFormTitle: "¿Necesitas ayuda con marketing digital? TrendMakers es tu solución",
+        labelName: "Nombre:",
+        labelCompany: "Empresa:",
+        labelProject: "Proyecto:",
+        submitContactForm: "Enviar",
+        contactSoon: "Nuestro equipo se pondrá en contacto con usted a la brevedad para discutir los detalles de su proyecto."
+    },
+    en: {
+        title: "TrendMakers",
+        subtitle: "Your digital Partner",
+        navInicio: "Home",
+        navAcerca: "Why choose us?",
+        navServicios: "Our Services",
+        navContacto: "Schedule a Call",
+        navEquipo: "Our Team",
+        introTitle: "Welcome to TrendMakers",
+        introText: "We offer personalized digital marketing solutions for your business.",
+        aboutTitle: "About Us",
+        aboutText: "We transform your vision into reality.<br><br>We start by listening and understanding your needs. We dive into your challenges to offer solutions and strategies that lead you to success.<br><br>With an agile and results-oriented approach, we not only meet objectives but also seek new challenges to drive your growth.<br><br>From the first contact to the final implementation, we combine digital marketing, design, programming, AI, and consulting to ensure your project not only works but thrives.",
+        servicesTitle: "Our Services",
+        servicesText: "We offer a wide range of digital marketing services...",
+        teamTitle: "Our team consists of:",
+        teamDetails: `
+            <div class="team-card">
+                <h3>Marketing Consultants</h3>
+                <p>They provide strategic advice to grow your business.</p>
+            </div>
+            <div class="team-card">
+                <h3>UX/UI, Graphic, and Multimedia Designers</h3>
+                <p>Responsible for creating innovative and user-friendly digital experiences.</p>
+            </div>
+            <div class="team-card">
+                <h3>Data Analysts</h3>
+                <p>Experts in interpreting metrics to drive informed decision-making.</p>
+            </div>
+            <div class="team-card">
+                <h3>Digital Campaign Specialists</h3>
+                <p>They optimize advertising campaigns to achieve the best results.</p>
+            </div>
+            <div class="team-card">
+                <h3>Web Developers</h3>
+                <p>Capable of creating high-performance websites and digital platforms.</p>
+            </div>
+            <div class="team-card">
+                <h3>SEO Specialists</h3>
+                <p>They improve organic positioning and increase your business's online visibility.</p>
+            </div>
+            <div class="team-card">
+                <h3>Business Intelligence Specialists</h3>
+                <p>They help transform data into valuable and actionable information.</p>
+            </div>
+            <div class="team-card">
+                <h3>Community Managers</h3>
+                <p>Experts in managing social networks to increase your brand's presence and engagement.</p>
+            </div>
+        `,
+        contactTitle: "Schedule a 1-on-1 Call",
+        scheduleCall: "Schedule a Call",
+        problemaButton: "Tell Us Your Project",
+        popupTitle: "Tell Us Your Project",
+        labelEmail: "Email:",
+        labelProblema: "Project:",
+        submitButton: "Submit",
+        footerText: "&copy; 2023 TrendMakers. All rights reserved.",
+        aboutUsText: "We are a global marketing agency dedicated to providing excellent services to clients all over the world. With a team of experts in various areas of digital marketing, we specialize in creating personalized strategies that cater to the specific needs of each business. Our mission is to drive growth and success for our clients through innovative and effective solutions. From social media management to the development of comprehensive advertising campaigns, we are committed to excellence and customer satisfaction in every project we undertake.",
+        serviceDetails: `
+            <div class="service-card">
+                <img src="logo1.png" alt="Marketing & Technology">
+                <h3>Marketing & Technology</h3>
+                <p>We use technological platforms and custom developments to optimize Marketing and Sales processes, implementing automation in commercial cycles.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Leads">
+                <h3>Leads</h3>
+                <p>Lead generation for SaaS platforms and service companies. Implementation and integration with CRM.</p>
+            </div>
+            <div class="service-card">
+                <img src="IA.jpg" alt="CHATGPT - AI">
+                <h3>CHATGPT - AI</h3>
+                <p>We empower your company by training all sectors and employees in ChatGPT and other Artificial Intelligence tools for day-to-day operations.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Design/Communication">
+                <h3>Design/Communication</h3>
+                <p>Design and layout of Websites, Landing Pages, WordPress, and Newsletters. Design of graphic pieces for social media, advertising, events. Community Management.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="E-Commerce">
+                <h3>E-Commerce</h3>
+                <p>We operate our own e-commerce platforms. We know how to do it. We accompany you from the idea to the launch in all stages of the project.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Consulting">
+                <h3>Consulting</h3>
+                <p>We provide consulting in Marketing & Technology (infrastructure or software) for your projects.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Data and Analytics">
+                <h3>Data and Analytics</h3>
+                <p>We turn data into valuable and actionable information. Using advanced tools, we interpret metrics and generate insights that drive strategic decisions.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Market Research">
+                <h3>Market Research</h3>
+                <p>We conduct comprehensive market research to understand trends and the needs of your target audience. We provide detailed analyses to identify opportunities and develop effective strategies.</p>
+            </div>
+            <div class="service-card">
+                <img src="logo1.png" alt="Web Development">
+                <h3>Web Development</h3>
+                <p>We develop high-performance websites and digital platforms tailored to your needs. From initial design to final implementation, we ensure an exceptional user experience and optimal performance.</p>
+            </div>
+        `,
+        contactFormTitle: "Need help with digital marketing? TrendMakers is your solution",
+        labelName: "Name:",
+        labelCompany: "Company:",
+        labelProject: "Project:",
+        submitContactForm: "Submit",
+        contactSoon: "Our team will reach out to you shortly to discuss the details of your project."
+    }
+};
+
+function setLanguage(language) {
+    const elementsToUpdate = {
+        'title': 'title',
+        'subtitle': 'subtitle',
+        'nav-inicio': 'navInicio',
+        'nav-acerca': 'navAcerca',
+        'nav-servicios': 'navServicios',
+        'nav-contacto': 'navContacto',
+        'nav-equipo': 'navEquipo',
+        'nav-problema': 'problemaButton', // Asegurarnos de incluir el botón "Contanos tu Proyecto"
+        'intro-title': 'introTitle',
+        'intro-text': 'introText',
+        'about-title': 'aboutTitle',
+        'about-text': 'aboutText',
+        'services-title': 'servicesTitle',
+        'services-text': 'servicesText',
+        'team-title': 'teamTitle',
+        'contact-title': 'contactTitle',
+        'schedule-call': 'scheduleCall',
+        'show-popup': 'problemaButton',
+        'popup-title': 'popupTitle',
+        'label-email': 'labelEmail',
+        'label-problema': 'labelProblema',
+        'submit-button': 'submitButton',
+        'footer-text': 'footerText',
+        'about-us-text': 'aboutUsText',
+        'contact-title': 'contactFormTitle',
+        'label-name': 'labelName',
+        'label-company': 'labelCompany',
+        'label-project': 'labelProject',
+        'submit-contact-form': 'submitContactForm',
+        'contact-soon': 'contactSoon'
+    };
+
+    for (const id in elementsToUpdate) {
+        const translationKey = elementsToUpdate[id];
+        const element = document.getElementById(id);
+        if (element) {
+            element.innerHTML = translations[language][translationKey];
+        }
+    }
+
+    const serviceDetailsElement = document.querySelector('.service-details');
+    if (serviceDetailsElement) {
+        serviceDetailsElement.innerHTML = translations[language].serviceDetails;
+    }
+
+    const teamDetailsElement = document.querySelector('.team-details');
+    if (teamDetailsElement) {
+        teamDetailsElement.innerHTML = translations[language].teamDetails;
+    }
+
+    // Actualizar los elementos de la barra de navegación
+    document.getElementById('nav-inicio').innerHTML = translations[language].navInicio;
+    document.getElementById('nav-acerca').innerHTML = translations[language].navAcerca;
+    document.getElementById('nav-servicios').innerHTML = translations[language].navServicios;
+    document.getElementById('nav-contacto').innerHTML = translations[language].navContacto;
+    document.getElementById('nav-equipo').innerHTML = translations[language].navEquipo;
+    document.getElementById('nav-problema').innerHTML = translations[language].problemaButton; // Actualizar el botón "Contanos tu Proyecto"
+}
+
+
+// Animaciones de entrada
+document.addEventListener('DOMContentLoaded', function() {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1 // Ajustar el umbral para activar la animación cuando el 10% del elemento esté visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Dejar de observar una vez que el elemento se ha hecho visible
+            }
+        });
+    }, observerOptions);
+
+    // Seleccionar todos los elementos que deben ser animados
+    const animatedElements = document.querySelectorAll('.animate__animated, .service-card');
+    animatedElements.forEach(element => {
+        observer.observe(element);
+    });
+
+    // Pop-up
+    const popup = document.getElementById('popup');
+    const showPopupButton = document.getElementById('show-popup');
+    const closePopupButton = document.getElementById('close-popup');
+
+    showPopupButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+    });
+
+    closePopupButton.addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    });
+
+    // Preloader
+    const preloader = document.getElementById('preloader');
+    const body = document.body;
+
+    window.addEventListener('load', function() {
+        preloader.style.display = 'none';
+        body.classList.remove('loading');
+        body.classList.add('loaded');
+    });
+
+    body.classList.add('loading');
+});
